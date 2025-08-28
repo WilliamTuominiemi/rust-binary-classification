961f6c1a891883f55e807ad938ce3812bc3b82ea
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-08-28 22:31:06 +0300

    update weights function

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,29 @@ fn compute_loss(y_true: f32, y_pred: f32) -> f32 {
     loss
 }
 
+fn update_weights(
+    w1: f32,
+    x1: f32,
+    b: f32,
+    w2: f32,
+    x2: f32,
+    y_true: f32,
+    y_pred: f32,
+    learning_rate: f32,
+) -> (f32, f32, f32) {
+    let error = y_pred - y_true;
+    let dw1 = error * x1;
+    let dw2 = error * x2;
+    let db = error;
+
+    let updated_w1 = w1 - learning_rate * dw1;
+    let updated_w2 = w2 - learning_rate * dw2;
+    let updated_b = b - learning_rate * db;
+
+    (updated_w1, updated_w2, updated_b)
+}
+
 fn main() {
-    let l = compute_loss(0.6, 0.8);
-    println!("{l}");
+    let (w1, w2, b) = update_weights(0.6, 0.8, 0.6, 0.8, 0.6, 0.8, 0.6, 0.8);
+    println!("{w1}, {w2}, {b}");
 }
