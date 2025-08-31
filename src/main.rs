28535204226a6322cf163436b5fe785739c9b671
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-08-31 18:42:36 +0300

    documentation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use rand::Rng;
 
+// Applies the sigmoid activation function, which maps any real number into the range (0, 1).
 fn sigmoid(z: f32) -> f32 {
     1.0 / (1.0 + (-z).exp())
 }
 
+// Performs a forward pass of a neuron. It computes a weighted sum of the inputs,
+// adds the bias, and passes the result through the sigmoid activation function.
 fn forward_pass(w1: f32, x1: f32, w2: f32, x2: f32, b: f32) -> f32 {
     let z: f32 = w1 * x1 + w2 * x2 + b;
     let a: f32 = sigmoid(z);
     a
 }
 
+// Updates the weights and bias using gradient descent. It calculates
+// the prediction error and adjusts the weights and bias in the opposite
+// direction of the gradient to reduce the error.
 fn update_weights(
     w1: f32,
     x1: f32,
@@ -32,6 +38,7 @@ fn update_weights(
     (updated_w1, updated_w2, updated_b)
 }
 
+// Ensures that the perceptron starts with non-zero parameters.
 fn initialize_weights() -> (f32, f32, f32) {
     let mut rng = rand::rng();
     let w1: f32 = rng.random_range(-0.5..0.5);
@@ -41,6 +48,8 @@ fn initialize_weights() -> (f32, f32, f32) {
     (w1, w2, b)
 }
 
+// The training process consists of multiple epochs where each input sample
+// is fed forward, and the weights/bias are updated using gradient descent.
 fn train(x: Vec<(f32, f32)>, y: Vec<f32>, epochs: i32, learning_rate: f32) -> (f32, f32, f32) {
     let mut w1: f32;
     let mut w2: f32;
@@ -66,6 +75,7 @@ fn train(x: Vec<(f32, f32)>, y: Vec<f32>, epochs: i32, learning_rate: f32) -> (f
     (w1, w2, b)
 }
 
+// Makes predictions using the trained perceptron and compares them to the true.
 fn predict(x: Vec<(f32, f32)>, y: Vec<f32>, w1: f32, w2: f32, b: f32) {
     for (index, (x1, x2)) in x.iter().enumerate() {
         let prediction: f32 = forward_pass(w1, *x1, w2, *x2, b);
